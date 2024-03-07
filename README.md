165262240c35a8c62dcf53658fa68793be4c86e2
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2024-03-07 11:51:04 -0500

    feat: Nueva rama con ejercicios para list comprehensions

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
-# Recursos de aprendizaje para el curso de POO en Python
+#List comprehensions
+> **List comprehensions** nos ayuda a simplificar la creación de nuestras listas en python, donde en vez de tener varias líneas de codigo definiendo el contenido que tendra la lista, podremos establecer de forma rapida, legible y en una sola linea, como estara construida nuestra lista.
+---
+> Hay varias formas de utilizar **List comprehensions**, las siguientes son las más comunes:
+<ul>
+  <li>
+      <h2><b>Declaración basica:<b/><h2/>
+      <code>[numero**2 for numero in range(5)]</code>
+  </li>
+    <li>
+      <h2><b>Declaración anidada:<b/><h2/>
+      <code>[[columna for columna in range(8)] for fila in range(8)]</code>
+  </li>
+  <li>
+      <h2><b>Declaración ciclos anidados:<b/><h2/>
+      <code>[(x,y) for x in range(5) for y in range(6,11)]</code>
+  </li>
+  <li>
+      <h2><b>Declaración con condicionales:<b/><h2/>
+      <code>[numero if i!2==0 else 0 for numero in range(1,300)]</code>
+  </li>
+</ul>
 
-## Introducción
-La **Programación Orientada a Objetos (POO)** es un paradigma de programación que utiliza objetos y sus interacciones para diseñar aplicaciones y programas informáticos. Utilizando Python, un lenguaje que facilita la POO con su sintaxis clara y su poderosa funcionalidad, los estudiantes pueden aprender este paradigma de manera efectiva.
 
-## Importancia de la POO en Python
-La POO es crucial en Python, ya que es uno de los lenguajes que permite un uso extensivo de este paradigma. Permite a los desarrolladores:
-- **Encapsular** datos y comportamientos en entidades modulares.
-- **Abstraer** complejidades mediante interfaces simples.
-- **Heredar** y extender funcionalidades de manera ordenada.
-- **Polimorfismo**: la capacidad de utilizar una entidad en múltiples formas.
-
-## Temas Avanzados en Python
-Además de los fundamentos de la POO, este curso cubrirá conceptos avanzados en Python, tales como:
-- **Sobrescritura de Métodos**: Modificar el comportamiento de métodos heredados.
-- **Dataclasses**: Simplificar la creación de clases y evitar la redundancia de código.
-- **List Comprehension**: Crear listas de manera concisa y eficiente.
-- **Dict Comprehension**: Construir diccionarios de forma compacta.
--**map, filter y reduce**: Cuando y como utilizar estas funciones que nos permiten realizar procesos sobre un iterable sin alterar su contenido original ni usar loops.
-
-## Estructura del Repositorio
-En este repositorio, encontrarás diferentes ramas correspondientes a cada uno de los temas mencionados. Dentro de cada rama, se incluyen los **enunciados** de ejercicios y desafíos, así como las **carpetas** con los archivos base donde deberás realizar tus soluciones. Explora las ramas para practicar y mejorar tus habilidades en POO y en los conceptos avanzados de Python.
-
+#**Ejercicios**
+>Los siguientes 8 ejercicios están ordenados por dificultad
+1. **Multiplos de 3 o 5**: Genera una lista de los múltiplos de 3 o 5 en el rango del 1 al 50 usando comprensión de listas.
+2. **Palabras más largas**: Dada la lista de palabras: `["Carmona","Carrusel","carro","Caro","Piel","Cafe","Miguel","Barro","Piso"]`, crea una nueva lista que contenga las palabras con más de 5 letras utilizando comprensión de listas.
+3. **Números primos**: Crea una lista de los números primos hasta 1001 usando comprensión de listas.
+4. **División** Crea una lista que contenga la división de los numeros del 15 al 30 entre los numeros del 2 al 5 ejm: 15/2 15/3 15/4 15/5 16/2 16/3 16/4 16/5
+5. **Matriz de identidad**: Genera una matriz de identidad de 5x5 utilizando comprensión de listas.
+6. **Producto cartesiano**: Dadas dos listas A= `[3,5,6,7,8]` y B=`[23,35,21,45,1]`, crea una lista de tuplas que representen todas las combinaciones posibles entre elementos de A y B utilizando comprensión de listas.
+7. **Generar números de Fibonacci**: Crea una lista que contenga los primeros 15 números de la secuencia de Fibonacci
+8. **Generar tabla de multiplicación pares**: Generar la tabla de multiplicación de 12 x 12, si la suma de sus digitos es par, se pone el numero, de lo contrario un 0
--- main.py
+++ main.py
@@ -1,16 +1,91 @@
-# This is a sample Python script.
+# SOLUCIÓN A LOS EJERCICIOS
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Ejercicio 1
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# resultado esperado: [3,5,6,9,10,12,15,18,20,21,24,25,27,30,33,35,36,39,40,42,45,48,50]
+def multiplos_de_3_5 ():
+    ...
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Ejercicio 2
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# resultado esperado: ["Carmona", "Carrusel", "Miguel"]
+def palabras_mas_largas ():
+    ...
+
+
+# Ejercicio 3
+
+# resultado esperado: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+def numeros_primo ():
+    ...
+
+
+# Ejercicio 4
+
+"""
+resultado esperado: estos son los valores esperados, recuerde que el resultado de la funcion es una lista con estos valores
+15 dividido por 2 es 7.5, por 3 es 5.0, por 4 es 3.75, por 5 es 3.0
+16 dividido por 2 es 8.0, por 3 es 5.33, por 4 es 4.0, por 5 es 3.2
+17 dividido por 2 es 8.5, por 3 es 5.67, por 4 es 4.25, por 5 es 3.4
+18 dividido por 2 es 9.0, por 3 es 6.0, por 4 es 4.5, por 5 es 3.6
+19 dividido por 2 es 9.5, por 3 es 6.33, por 4 es 4.75, por 5 es 3.8
+20 dividido por 2 es 10.0, por 3 es 6.67, por 4 es 5.0, por 5 es 4.0
+21 dividido por 2 es 10.5, por 3 es 7.0, por 4 es 5.25, por 5 es 4.2
+22 dividido por 2 es 11.0, por 3 es 7.33, por 4 es 5.5, por 5 es 4.4
+23 dividido por 2 es 11.5, por 3 es 7.67, por 4 es 5.75, por 5 es 4.6
+24 dividido por 2 es 12.0, por 3 es 8.0, por 4 es 6.0, por 5 es 4.8
+25 dividido por 2 es 12.5, por 3 es 8.33, por 4 es 6.25, por 5 es 5.0
+26 dividido por 2 es 13.0, por 3 es 8.67, por 4 es 6.5, por 5 es 5.2
+27 dividido por 2 es 13.5, por 3 es 9.0, por 4 es 6.75, por 5 es 5.4
+28 dividido por 2 es 14.0, por 3 es 9.33, por 4 es 7.0, por 5 es 5.6
+29 dividido por 2 es 14.5, por 3 es 9.67, por 4 es 7.25, por 5 es 5.8
+30 dividido por 2 es 15.0, por 3 es 10.0, por 4 es 7.5, por 5 es 6.0
+"""
+
+
+def division ():
+    ...
+
+
+# Ejercicio 5
+
+# resultad esperado: [[1 0 0 0 0], [0 1 0 0 0], [0 0 1 0 0], [0 0 0 1 0], [0 0 0 0 1]]
+
+def matriz_identidad ():
+    ...
+
+
+# Ejercicio 6
+
+# resultado esperado: [(3,23), (3,35), (3,21), (3,45), (3,1), (5,23), (5,35), (5,21), (5,45), (5,1), (6,23), (6,35), (6,21), (6,45), (6,1), (7,23), (7,35), (7,21), (7,45), (7,1), (8,23), (8,35), (8,21), (8,45), (8,1)]
+
+def combinaciones ():
+    ...
+
+# Ejercicio 7
+
+# resultado esperado: [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610]
+
+def fibonacci ():
+    ...
+
+# Ejercicio 8
+
+"""
+el resultado esperado es: 
+[0, 2, 0, 4, 0, 6, 0, 8, 0, 0, 11, 0],
+ [2, 4, 6, 8, 0, 0, 0, 0, 0, 20, 22, 24],
+ [0, 6, 0, 0, 15, 0, 0, 24, 0, 0, 33, 0],
+ [4, 8, 0, 0, 20, 24, 28, 0, 0, 40, 44, 48],
+ [0, 0, 15, 20, 0, 0, 35, 40, 0, 0, 55, 60],
+ [6, 0, 0, 24, 0, 0, 42, 48, 0, 60, 66, 0],
+ [0, 0, 0, 28, 35, 42, 0, 0, 0, 0, 77, 84],
+ [8, 0, 24, 0, 40, 48, 0, 64, 0, 80, 88, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0],
+ [0, 20, 0, 40, 0, 60, 0, 80, 0, 0, 110, 0],
+ [11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132],
+ [0, 24, 0, 48, 60, 0, 84, 0, 0, 0, 132, 0]]
+"""
+def tabla_multiplicacion_pares():
+    ...
